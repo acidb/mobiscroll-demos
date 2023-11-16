eb461285b5a056ca57c499cabb7724852cadd5d5
Istvan Csiszer <istvan.csiszer@mobiscroll.com>
2023-11-16 12:52:09 +0200

    vue-ts

---
+++ vue-ts/src/demos/eventcalendar/scheduler/work-week-hours.ts.vue
@@ -0,0 +1,98 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import {
+  MbscEventcalendar,
+  setOptions,
+  getJson,
+  MbscToast /* localeImport */
+} from '@mobiscroll/vue'
+import type {
+  MbscCalendarEvent,
+  MbscEventcalendarView,
+  MbscEventCreateFailedEvent,
+  MbscEventUpdateFailedEvent
+} from '@mobiscroll/vue'
+
+setOptions({
+  // locale,
+  // theme
+})
+
+const myEvents = ref<MbscCalendarEvent[]>([])
+const toastMessage = ref<string>('')
+const isToastOpen = ref<boolean>(false)
+
+const myView: MbscEventcalendarView = {
+  schedule: {
+    type: 'week',
+    startDay: 1,
+    endDay: 5,
+    startTime: '09:00',
+    endTime: '18:00'
+  }
+}
+
+const myInvalid = [
+  {
+    start: '12:00',
+    end: '13:00',
+    title: 'Lunch break',
+    type: 'lunch',
+    recurring: {
+      repeat: 'weekly',
+      weekDays: 'MO,TU,WE,TH,FR'
+    }
+  }
+]
+
+function handleEventCreateFailed(event: MbscEventCreateFailedEvent) {
+  if (event.invalid.type == 'lunch') {
+    toastMessage.value = "Can't create this task on lunch break."
+    isToastOpen.value = true
+  }
+}
+
+function handleEventUpdateFailed(event: MbscEventUpdateFailedEvent) {
+  if (event.invalid.type == 'lunch') {
+    toastMessage.value = "Can't schedule this task on lunch break."
+    isToastOpen.value = true
+  }
+}
+
+function handleToastClose() {
+  isToastOpen.value = false
+}
+
+onMounted(() => {
+  getJson(
+    'https://trial.mobiscroll.com/workday-events/',
+    (events: MbscCalendarEvent[]) => {
+      myEvents.value = events
+    },
+    'jsonp'
+  )
+})
+</script>
+
+<template>
+  <MbscEventcalendar
+    :view="myView"
+    :data="myEvents"
+    :invalid="myInvalid"
+    :dragToCreate="true"
+    :dragToMove="true"
+    @event-create-failed="handleEventCreateFailed"
+    @event-update-failed="handleEventUpdateFailed"
+  />
+  <MbscToast :message="toastMessage" :isOpen="isToastOpen" @close="handleToastClose" />
+</template>
+
+<style>
+/*<hidden>*/
+
+.demo-work-week-hours {
+  height: 100%;
+}
+
+/*</hidden>*/
+</style>
---
+++ vue-ts/src/router/index.ts
@@ -0,0 +1,35 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import WorkWeekHours from '../demos/eventcalendar/scheduler/work-week-hours.ts.vue'
+import CompareResources from '../demos/eventcalendar/timeline/compare-resources.ts.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/timeline/compare-resources',
+      name: 'compare-resources',
+      component: CompareResources
+    },
+    {
+      path: '/schedule/work-week-hours',
+      name: 'work-week-hours',
+      component: WorkWeekHours
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
+    }
+  ]
+})
+
+export default router
