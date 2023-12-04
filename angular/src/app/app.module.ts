a6fc3b797b87a15e80ff9d4c49f1c80ff3046cdc
Istvan Csiszer <istvan.csiszer@mobiscroll.com>
2023-12-04 19:07:59 +0200

    progress

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { FormsModule } from '@angular/forms';
+import { MbscModule } from '@mobiscroll/angular';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -8,7 +10,9 @@ import { AppComponent } from './app.component';
   declarations: [
     AppComponent
   ],
-  imports: [
+  imports: [  
+    FormsModule,   
+    MbscModule, 
     BrowserModule,
     AppRoutingModule
   ],
