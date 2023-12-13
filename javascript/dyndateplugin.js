3a3e1d66ea14cc9853a0367df2e8bf1507927fcd
Istvan Csiszer <istvan.csiszer@mobiscroll.com>
2023-12-13 14:33:00 +0200

    javascript datetime initial

--- javascript/dyndateplugin.js
+++ javascript/dyndateplugin.js
@@ -14,12 +14,51 @@ export default function myPlugin() {
     },
   };
 }
-
+const now = new Date();
 const compileFileToJS = (src) => {
-  //   const matches = src.match('dyndatetime[sS]*?)');
-  //   matches.forEach((element) => {
-  //     src.replace(element, 'teeeeest');
-  //   });
+  var str = src.replace(/:.*dyndatetime.*/g, function (i, match) {
+    return parseDatestring(i);
+  });
+  console.log(str);
+  return str;
+};
 
-  return src;
+const parseDatestring = (s) => {
+  s = s.replace(/dyndatetime/, '');
+  s = s.replace(/\(/, '');
+  s = s.replace(/\)/, ''); //ymdhi
+  s = s.replace(/y/, now.getFullYear());
+  s = s.replace(/m/, now.getMonth());
+  s = s.replace(/d/, now.getDate());
+  s = s.replace(/h/, now.getHours());
+  s = s.replace(/i/, now.getMinutes());
+  s = s.replace(/'(.*)'/, function (i, match) {
+    const dateDict = {
+      0: 0,
+      1: 0,
+      2: 0,
+      3: 0,
+      4: 0,
+    };
+    const date = i.replace(/'/g, '');
+    let dateArray = date.split(',');
+    dateArray.forEach((i, index) => {
+      const plus = i.includes('+');
+      const minus = i.includes('-');
+      const splittedNum = i.split(/[\+|\-]/);
+      let num = 0;
+      splittedNum.forEach((element) => {
+        if (plus) {
+          return (num += parseInt(element));
+        }
+        if (minus) {
+          return index === 0 ? (num += parseInt(element)) : (num -= parseInt(element));
+        }
+        num += parseInt(element);
+      });
+      return (dateDict[index] = num);
+    });
+    return "'" + new Date(dateDict[0], dateDict[1], dateDict[2], dateDict[3], dateDict[4]).toISOString() + "'";
+  });
+  return s;
 };
