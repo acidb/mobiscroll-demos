816c2238c71859f489769fd4f2ccfa1ee59e535f
Istvan Csiszer <istvan.csiszer@mobiscroll.com>
2023-12-14 14:27:00 +0200

    dts plugin out

--- react-ts/vite.config.ts
+++ react-ts/vite.config.ts
@@ -1,15 +1,8 @@
 import { defineConfig } from 'vite';
-import dts from 'vite-plugin-dts';
 import react from '@vitejs/plugin-react';
 import dyndateplugin from './dyndateplugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    dyndateplugin(),
-    dts({
-      include: ['./dyndateplugin'],
-    }),
-  ],
+  plugins: [react(), dyndateplugin()],
 });
--- vue-ts/vite.config.ts
+++ vue-ts/vite.config.ts
@@ -1,21 +1,13 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
-import dts from 'vite-plugin-dts'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import dyndateplugin from './dyndateplugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    dyndateplugin(),
-    dts({
-      include: ['./dyndateplugin']
-    })
-  ],
+  plugins: [vue(), vueJsx(), dyndateplugin()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
