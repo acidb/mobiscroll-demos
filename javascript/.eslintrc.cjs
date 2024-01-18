18ace9ec3250a629d701bd0b49812fe548c5c8e4
Halmen Istvan <isti@mobiscroll.com>
2024-01-18 15:09:33 +0200

    JavaScript and jQuery app lint config

---
+++ javascript/.eslintrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'prettier'],
+  plugins: ['import', 'promise'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {},
+};
--- javascript/dyndateplugin.js
+++ javascript/dyndateplugin.js
@@ -1,13 +1,10 @@
-const fileRegex = /\.(js)$/;
-
 export default function myPlugin() {
   return {
     name: 'transform-file',
-
     transform(src, id) {
-      if (fileRegex.test(id)) {
+      if (/\.(js)$/.test(id)) {
         return {
-          code: compileFileToJS(src),
+          code: replaceDynamicDates(src),
           map: null, // provide source map if available
         };
       }
@@ -15,23 +12,23 @@ export default function myPlugin() {
   };
 }
 const now = new Date();
-const compileFileToJS = (src) => {
-  const str = src.replace(/'dyndatetime\(([^)])*\)'/g, function (i, match) {
+
+const replaceDynamicDates = (src) => {
+  return src.replace(/['|"]dyndatetime\(([^)])*\)['|"]/g, function (i) {
     return parseDatestring(i);
   });
-  return str;
 };
 
 const parseDatestring = (s) => {
   s = s.replace(/dyndatetime/, '');
   s = s.replace(/\(/, '');
-  s = s.replace(/\)/, ''); //ymdhi
+  s = s.replace(/\)/, '');
   s = s.replace(/y/, now.getFullYear());
-  s = s.replace(/m/, now.getMonth());
+  s = s.replace(/m/, now.getMonth() + 1);
   s = s.replace(/d/, now.getDate());
   s = s.replace(/h/, now.getHours());
   s = s.replace(/i/, now.getMinutes());
-  s = s.replace(/['|"](.*)['|"]/, function (i, match) {
+  s = s.replace(/['|"](.*)['|"]/, function (i) {
     const dateDict = {
       0: 0,
       1: 0,
@@ -42,22 +39,37 @@ const parseDatestring = (s) => {
     const date = i.replace(/['|"]/g, '');
     const dateArray = date.split(',');
     dateArray.forEach((i, index) => {
-      const plus = i.includes('+');
-      const minus = i.includes('-');
-      const splittedNum = i.split(/[\+|\-]/);
-      let num = 0;
-      splittedNum.forEach((element) => {
-        if (plus) {
-          return (num += parseInt(element));
-        }
-        if (minus) {
-          return index === 0 ? (num += parseInt(element)) : (num -= parseInt(element));
-        }
-        num += parseInt(element);
-      });
-      return (dateDict[index] = num);
+      const splittedNum = i.split(/[/+|/-]/);
+      if (splittedNum.length > 1) {
+        const minus = i.indexOf('-') !== -1;
+        dateDict[index] = minus ? +splittedNum[0] - +splittedNum[1] : +splittedNum[0] + +splittedNum[1];
+      } else {
+        dateDict[index] = +splittedNum[0];
+      }
     });
-    return "'" + new Date(dateDict[0], dateDict[1], dateDict[2], dateDict[3], dateDict[4]).toISOString() + "'";
+    const dd = new Date(dateDict[0], dateDict[1] - 1, dateDict[2], dateDict[3], dateDict[4]);
+    const y = dd.getFullYear();
+    const m = dd.getMonth() + 1;
+    const d = dd.getDate();
+    const h = dd.getHours();
+    const mm = dd.getMinutes();
+    return (
+      "'" +
+      y +
+      '-' +
+      (m < 10 ? '0' : '') +
+      m +
+      '-' +
+      (d < 10 ? '0' : '') +
+      d +
+      'T' +
+      (h < 10 ? '0' : '') +
+      h +
+      ':' +
+      (mm < 10 ? '0' : '') +
+      mm +
+      "'"
+    );
   });
   return s;
 };
