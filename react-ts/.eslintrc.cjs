48d94b202443ea16001e6506db2b80a40f4f57bf
Halmen Istvan <isti@mobiscroll.com>
2024-01-24 18:06:31 +0200

    Setup import order for the React Typescript app

--- react-ts/.eslintrc.cjs
+++ react-ts/.eslintrc.cjs
@@ -4,8 +4,9 @@ module.exports = {
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended'],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['import', 'react-refresh'],
   rules: {
+    'import/order': ['warn', { alphabetize: { order: 'asc', caseInsensitive: true } }],
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
   },
 };
