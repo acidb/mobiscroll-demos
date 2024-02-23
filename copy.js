847e91d75e6c352ef683ec5e60dd24d37b060a87
Halmen Istvan <isti@mobiscroll.com>
2024-02-23 13:15:36 +0200

    Copy script fixes

--- copy.js
+++ copy.js
@@ -1,14 +1,14 @@
 const fse = require('fs-extra');
 const config = require('./config.json');
 
-const filter = (src) => !/node_modules|dist/.test(src);
+const filter = (src) => !/node_modules|dist|.angular/.test(src);
 
 const opt = { filter };
 
-fse.copy('mobiscroll-demos-angular', config.demosPathPublic + './mobiscroll-demos-angular', opt);
-fse.copy('mobiscroll-demos-javascript', config.demosPathPublic + './mobiscroll-demos-javascript', opt);
-fse.copy('mobiscroll-demos-jquery', config.demosPathPublic + './mobiscroll-demos-jquery', opt);
-fse.copy('mobiscroll-demos-react', config.demosPathPublic + './mobiscroll-demos-react', opt);
-fse.copy('mobiscroll-demos-react-ts', config.demosPathPublic + './mobiscroll-demos-react-ts', opt);
-fse.copy('mobiscroll-demos-vue', config.demosPathPublic + './mobiscroll-demos-vue', opt);
-fse.copy('mobiscroll-demos-vue-ts', config.demosPathPublic + './mobiscroll-demos-vue-ts', opt);
+fse.copy('mobiscroll-demos-angular', config.demosPathPublic + '/mobiscroll-demos-angular', opt);
+fse.copy('mobiscroll-demos-javascript', config.demosPathPublic + '/mobiscroll-demos-javascript', opt);
+fse.copy('mobiscroll-demos-jquery', config.demosPathPublic + '/mobiscroll-demos-jquery', opt);
+fse.copy('mobiscroll-demos-react', config.demosPathPublic + '/mobiscroll-demos-react', opt);
+fse.copy('mobiscroll-demos-react-ts', config.demosPathPublic + '/mobiscroll-demos-react-ts', opt);
+fse.copy('mobiscroll-demos-vue', config.demosPathPublic + '/mobiscroll-demos-vue', opt);
+fse.copy('mobiscroll-demos-vue-ts', config.demosPathPublic + '/mobiscroll-demos-vue-ts', opt);
