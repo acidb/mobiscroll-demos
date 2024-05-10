1793f17107a45b6043ea6c9d56ac66899a82c1cb
Istvan Csiszer <istvan.csiszer@mobiscroll.com>
2024-05-10 16:26:25 +0300

    angular demo1,2

---
+++ mobiscroll-demos-angular/src/demos/eventcalendar/timeline/check-list-tasks-within-events/check-list-tasks-within-events.ts
@@ -0,0 +1,120 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {
+  MbscCalendarEvent,
+  MbscEventClickEvent,
+  MbscEventcalendarOptions,
+  MbscResource,
+  Notifications,
+  setOptions /* localeImport */,
+} from '@mobiscroll/angular';
+
+setOptions({
+  // locale,
+  // theme
+});
+
+@Component({
+  selector: 'app-timeline-timeline-resource-height',
+  styleUrl: './check-list-tasks-within-events.css',
+  encapsulation: ViewEncapsulation.None,
+  templateUrl: './check-list-tasks-within-events.html',
+  providers: [Notifications],
+})
+export class AppComponent implements OnInit {
+  constructor(
+    private http: HttpClient,
+    private notify: Notifications,
+    // public notify: Notifications
+  ) {}
+
+  calendarOptions: MbscEventcalendarOptions = {
+    dragToCreate: true,
+    clickToCreate: true,
+    dragToMove: true,
+    dragToResize: true,
+    view: {
+      timeline: {
+        type: 'week',
+        eventHeight: 'variable',
+        eventList: true,
+      },
+    },
+    extendDefaultEvent: () => ({
+      title: 'New Event',
+      tasks: ['Default task'],
+    }),
+  };
+
+  myEvents: MbscCalendarEvent[] = [];
+
+  myResources: MbscResource[] = [
+    {
+      id: 1,
+      name: 'Site Plumbing Squad',
+      description: 'Elite construction plumbers ensuring flawless pipeline installations on every site.',
+      color: '#01adff',
+    },
+    {
+      id: 2,
+      name: 'Pipeline Builders',
+      description: 'Constructing fluid pathways, one precision connection at a time.',
+      color: '#239a21',
+    },
+    {
+      id: 3,
+      name: 'Blueprint Plumbers',
+      description: 'Turning plans into precise pipelines with expert craftsmanship.',
+      color: '#ff4600',
+    },
+    {
+      id: 4,
+      name: 'Site Supply Specialists',
+      description: 'Delivering essential plumbing materials promptly to construction sites',
+      color: '#4981d6',
+    },
+    {
+      id: 5,
+      name: 'Infrastructure Installers',
+      description: ' Building the backbone of modern plumbing systems efficiently.',
+      color: '#f1e920',
+    },
+    {
+      id: 6,
+      name: 'Steel Sinks Squad',
+      description: 'Installing robust sinks for industrial and commercial settings.',
+      color: '#f7961e',
+    },
+  ];
+
+  handleEventClick(args: MbscEventClickEvent): void {
+    if (args.domEvent.target.id === 'demo-check-list-tasks-add-button') {
+      const ev = args.event;
+      const updatedTasks = ev['tasks'].slice();
+      const index = this.myEvents.findIndex((event) => event.id === ev.id);
+      const newEventList = [...this.myEvents];
+      this.notify.prompt({
+        title: 'Add new task to ' + ev.title,
+        callback: function (value) {
+          if (value) {
+            updatedTasks.push(value);
+            ev['tasks'] = updatedTasks;
+            newEventList.splice(index, 1, ev);
+          }
+        },
+      });
+      this.myEvents = newEventList;
+      this.notify.toast({
+        duration: 3000,
+        message: 'Tasks updated for ' + ev.title,
+      });
+    }
+  }
+
+  ngOnInit(): void {
+    //TODO change the link to trial
+    this.http.jsonp<MbscCalendarEvent[]>('https://trialdev.mobiscroll.com/events-check-list-tasks/', 'callback').subscribe((resp) => {
+      this.myEvents = resp;
+    });
+  }
+}
