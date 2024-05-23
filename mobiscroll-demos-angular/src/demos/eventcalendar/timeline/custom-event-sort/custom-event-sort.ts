1291aea20125207155fd256d23d06e9095ca31a5
szily20 <szily20@gmail.com>
2024-05-23 08:53:55 +0300

    Added custom-event-sort demos for schedule and timeline

---
+++ mobiscroll-demos-angular/src/demos/eventcalendar/timeline/custom-event-sort/custom-event-sort.ts
@@ -0,0 +1,154 @@
+import { Component } from '@angular/core';
+import { MbscCalendarEvent, MbscEventcalendarView, setOptions /* localeImport */ } from '@mobiscroll/angular';
+
+setOptions({
+  // locale,
+  // theme
+});
+
+const now = new Date();
+const day = now.getDay();
+const monday = now.getDate() - day + (day == 0 ? -6 : 1);
+
+@Component({
+  selector: 'app-eventcalendar-custom-event-sort',
+  templateUrl: './custom-event-sort.html',
+})
+export class AppComponent {
+  myEvents: MbscCalendarEvent[] = [
+    {
+      start: new Date(now.getFullYear(), now.getMonth(), monday),
+      title: 'Kate OFF (PROPOSED)',
+      color: '#e7b300',
+      allDay: true,
+      accepted: false,
+      order: 1,
+    },
+    {
+      start: new Date(now.getFullYear(), now.getMonth(), monday),
+      title: 'John OFF (APPROVED)',
+      color: '#00ca10',
+      allDay: true,
+      accepted: true,
+      order: 2,
+    },
+    {
+      start: new Date(now.getFullYear(), now.getMonth(), monday),
+      title: 'Mark OFF (PROPOSED)',
+      color: '#e7b300',
+      allDay: true,
+      accepted: false,
+      order: 1,
+    },
+    {
+      start: new Date(now.getFullYear(), now.getMonth(), monday),
+      title: 'Emma OFF (PROPOSED)',
+      color: '#e7b300',
+      allDay: true,
+      accepted: false,
+      order: 1,
+    },
+    {
+      start: new Date(now.getFullYear(), now.getMonth(), monday + 1),
+      title: 'Mark OFF (APPROVED)',
+      color: '#00ca10',
+      allDay: true,
+      accepted: true,
+      order: 2,
+    },
+    {
+      start: new Date(now.getFullYear(), now.getMonth(), monday + 1),
+      title: 'Carol OFF (PROPOSED)',
+      color: '#e7b300',
+      allDay: true,
+      accepted: false,
+      order: 1,
+    },
+    {
+      start: new Date(now.getFullYear(), now.getMonth(), monday + 2),
+      title: 'Luke OFF (PROPOSED)',
+      color: '#e7b300',
+      allDay: true,
+      accepted: false,
+      order: 1,
+    },
+    {
+      start: new Date(now.getFullYear(), now.getMonth(), monday + 2),
+      title: 'Carol OFF (APPROVED)',
+      color: '#00ca10',
+      allDay: true,
+      accepted: true,
+      order: 2,
+    },
+    {
+      start: new Date(now.getFullYear(), now.getMonth(), monday + 2),
+      title: 'Kate OFF (APPROVED)',
+      color: '#00ca10',
+      allDay: true,
+      accepted: true,
+      order: 2,
+    },
+    {
+      start: new Date(now.getFullYear(), now.getMonth(), monday + 2),
+      title: 'Dean OFF (PROPOSED)',
+      color: '#e7b300',
+      allDay: true,
+      accepted: false,
+      order: 1,
+    },
+    {
+      start: new Date(now.getFullYear(), now.getMonth(), monday + 2),
+      title: 'Emma OFF (APPROVED)',
+      color: '#00ca10',
+      allDay: true,
+      accepted: true,
+      order: 2,
+    },
+    {
+      start: new Date(now.getFullYear(), now.getMonth(), monday + 2),
+      title: 'Jason OFF (APPROVED)',
+      color: '#00ca10',
+      allDay: true,
+      accepted: true,
+      order: 2,
+    },
+    {
+      start: new Date(now.getFullYear(), now.getMonth(), monday + 3),
+      title: 'Jason OFF (APPROVED)',
+      color: '#00ca10',
+      allDay: true,
+      accepted: true,
+      order: 2,
+    },
+    {
+      start: new Date(now.getFullYear(), now.getMonth(), monday + 4),
+      title: 'Ryan OFF (PROPOSED)',
+      color: '#e7b300',
+      allDay: true,
+      accepted: false,
+      order: 1,
+    },
+    {
+      start: new Date(now.getFullYear(), now.getMonth(), monday + 4),
+      title: 'John OFF (APPROVED)',
+      color: '#00ca10',
+      allDay: true,
+      accepted: true,
+      order: 2,
+    },
+    {
+      start: new Date(now.getFullYear(), now.getMonth(), monday + 4),
+      title: 'Dean OFF (PROPOSED)',
+      color: '#e7b300',
+      allDay: true,
+      accepted: false,
+      order: 1,
+    },
+  ];
+
+  view: MbscEventcalendarView = {
+    timeline: {
+      type: 'week',
+    },
+  };
+}
---
+++ mobiscroll-demos-vue-ts/src/demos/eventcalendar/scheduler/custom-event-sort/custom-event-sort.vue
@@ -0,0 +1,155 @@
+<script setup lang="ts">
+import { MbscEventcalendar, setOptions /* localeImport */ } from '@mobiscroll/vue'
+import type { MbscCalendarEvent, MbscEventcalendarView } from '@mobiscroll/vue'
+
+setOptions({
+  // locale,
+  // theme
+})
+
+const now = new Date()
+const day = now.getDay()
+const monday = now.getDate() - day + (day == 0 ? -6 : 1)
+
+const myEvents: MbscCalendarEvent[] = [
+  {
+    start: new Date(now.getFullYear(), now.getMonth(), monday),
+    title: 'Kate OFF (PROPOSED)',
+    color: '#e7b300',
+    allDay: true,
+    accepted: false,
+    order: 1
+  },
+  {
+    start: new Date(now.getFullYear(), now.getMonth(), monday),
+    title: 'John OFF (APPROVED)',
+    color: '#00ca10',
+    allDay: true,
+    accepted: true,
+    order: 2
+  },
+  {
+    start: new Date(now.getFullYear(), now.getMonth(), monday),
+    title: 'Mark OFF (PROPOSED)',
+    color: '#e7b300',
+    allDay: true,
+    accepted: false,
+    order: 1
+  },
+  {
+    start: new Date(now.getFullYear(), now.getMonth(), monday),
+    title: 'Emma OFF (PROPOSED)',
+    color: '#e7b300',
+    allDay: true,
+    accepted: false,
+    order: 1
+  },
+  {
+    start: new Date(now.getFullYear(), now.getMonth(), monday + 1),
+    title: 'Mark OFF (APPROVED)',
+    color: '#00ca10',
+    allDay: true,
+    accepted: true,
+    order: 2
+  },
+  {
+    start: new Date(now.getFullYear(), now.getMonth(), monday + 1),
+    title: 'Carol OFF (PROPOSED)',
+    color: '#e7b300',
+    allDay: true,
+    accepted: false,
+    order: 1
+  },
+  {
+    start: new Date(now.getFullYear(), now.getMonth(), monday + 2),
+    title: 'Luke OFF (PROPOSED)',
+    color: '#e7b300',
+    allDay: true,
+    accepted: false,
+    order: 1
+  },
+  {
+    start: new Date(now.getFullYear(), now.getMonth(), monday + 2),
+    title: 'Carol OFF (APPROVED)',
+    color: '#00ca10',
+    allDay: true,
+    accepted: true,
+    order: 2
+  },
+  {
+    start: new Date(now.getFullYear(), now.getMonth(), monday + 2),
+    title: 'Kate OFF (APPROVED)',
+    color: '#00ca10',
+    allDay: true,
+    accepted: true,
+    order: 2
+  },
+  {
+    start: new Date(now.getFullYear(), now.getMonth(), monday + 2),
+    title: 'Dean OFF (PROPOSED)',
+    color: '#e7b300',
+    allDay: true,
+    accepted: false,
+    order: 1
+  },
+  {
+    start: new Date(now.getFullYear(), now.getMonth(), monday + 2),
+    title: 'Emma OFF (APPROVED)',
+    color: '#00ca10',
+    allDay: true,
+    accepted: true,
+    order: 2
+  },
+  {
+    start: new Date(now.getFullYear(), now.getMonth(), monday + 2),
+    title: 'Jason OFF (APPROVED)',
+    color: '#00ca10',
+    allDay: true,
+    accepted: true,
+    order: 2
+  },
+  {
+    start: new Date(now.getFullYear(), now.getMonth(), monday + 3),
+    title: 'Jason OFF (APPROVED)',
+    color: '#00ca10',
+    allDay: true,
+    accepted: true,
+    order: 2
+  },
+  {
+    start: new Date(now.getFullYear(), now.getMonth(), monday + 4),
+    title: 'Ryan OFF (PROPOSED)',
+    color: '#e7b300',
+    allDay: true,
+    accepted: false,
+    order: 1
+  },
+  {
+    start: new Date(now.getFullYear(), now.getMonth(), monday + 4),
+    title: 'John OFF (APPROVED)',
+    color: '#00ca10',
+    allDay: true,
+    accepted: true,
+    order: 2
+  },
+  {
+    start: new Date(now.getFullYear(), now.getMonth(), monday + 4),
+    title: 'Dean OFF (PROPOSED)',
+    color: '#e7b300',
+    allDay: true,
+    accepted: false,
+    order: 1
+  }
+]
+
+const myView: MbscEventcalendarView = {
+  schedule: {
+    type: 'week'
+  }
+}
+</script>
+
+<template>
+  <!-- dragOptions -->
+  <MbscEventcalendar :view="myView" :data="myEvents" />
+</template>
