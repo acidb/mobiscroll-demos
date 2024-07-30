2bb45bf189f71f8fd5ea67dd13964b8f49214cf8
Istvan Csiszer <istvan.csiszer@mobiscroll.com>
2024-07-30 16:37:14 +0300

    vue-ts

--- mobiscroll-demos-vue-ts/src/demos/eventcalendar/calendar-view/property-booking-calendar/property-booking-calendar.vue
+++ mobiscroll-demos-vue-ts/src/demos/eventcalendar/calendar-view/property-booking-calendar/property-booking-calendar.vue
@@ -1,41 +1,171 @@
 <script setup lang="ts">
-import { getJson, MbscEventcalendar, setOptions /* localeImport */ } from '@mobiscroll/vue'
-import type {
-  MbscCalendarEvent,
-  MbscEventcalendarView,
-  MbscResource
-} from '@mobiscroll/vue'
-import { onMounted, ref } from 'vue'
+import { MbscEventcalendar, setOptions /* localeImport */ } from '@mobiscroll/vue'
+import type { MbscCalendarEvent, MbscEventcalendarView, MbscNewEventData } from '@mobiscroll/vue'
 
 setOptions({
   // locale,
   // theme
 })
 
-const myEvents = ref<MbscCalendarEvent[]>([])
+const airBnB = 'https://img.mobiscroll.com/demos/air-bnb-icon.png'
+const booking = 'https://img.mobiscroll.com/demos/booking-icon.png'
+const makeMyTrip = 'https://img.mobiscroll.com/demos/make-my-trip-icon.png'
 
-const myResources: MbscResource[] = []
+const myEvents: MbscCalendarEvent[] = [
+  {
+    start: 'dyndatetime(y,m,d-18,12)',
+    end: 'dyndatetime(y,m,d-15,12)',
+    icon: airBnB,
+    title: 'Mary Smith',
+    color: '#fffa61'
+  },
+  {
+    start: 'dyndatetime(y,m,d-7,12)',
+    end: 'dyndatetime(y,m,d-1,12)',
+    title: 'James Johnson',
+    icon: airBnB,
+    color: '#fffa61'
+  },
+  {
+    start: 'dyndatetime(y,m,d-12,12)',
+    end: 'dyndatetime(y,m,d-9,12)',
+    title: 'Patricia Williams',
+    icon: airBnB,
+    color: '#fffa61'
+  },
+  {
+    start: 'dyndatetime(y,m,d+3,12)',
+    end: 'dyndatetime(y,m,d+5,12)',
+    title: 'Michael Brown',
+    icon: airBnB,
+    color: '#fffa61'
+  },
+  {
+    start: 'dyndatetime(y,m,d+10,12)',
+    end: 'dyndatetime(y,m,d+11,12)',
+    title: 'Jennifer Jones',
+    icon: booking,
+    color: '#ff9999'
+  },
+  {
+    start: 'dyndatetime(y,m,d,12)',
+    end: 'dyndatetime(y,m,d+3,12)',
+    title: 'Robert Garcia',
+    icon: booking,
+    color: '#ff9999'
+  },
+  {
+    start: 'dyndatetime(y,m,d+19,12)',
+    end: 'dyndatetime(y,m,d+20,12)',
+    title: 'Linda Miller',
+    icon: booking,
+    color: '#ff9999'
+  },
+  {
+    start: 'dyndatetime(y,m,d+26,12)',
+    end: 'dyndatetime(y,m,d+27,12)',
+    title: 'John Davis',
+    icon: booking,
+    color: '#ff9999'
+  },
+  {
+    start: 'dyndatetime(y,m,d-15,12)',
+    end: 'dyndatetime(y,m,d-13,12)',
+    title: 'Elizabeth Rodriguez',
+    icon: makeMyTrip,
+    color: '#a0efaa'
+  },
+  {
+    start: 'dyndatetime(y,m,d+13,12)',
+    end: 'dyndatetime(y,m,d+15,12)',
+    title: 'David Martinez',
+    icon: makeMyTrip,
+    color: '#a0efaa'
+  },
+  {
+    start: 'dyndatetime(y,m,d+16,12)',
+    end: 'dyndatetime(y,m,d+18,12)',
+    title: 'Barbara Wilson',
+    icon: makeMyTrip,
+    color: '#a0efaa'
+  },
+  {
+    start: 'dyndatetime(y,m,d-9,12)',
+    end: 'dyndatetime(y,m,d-8,12)',
+    title: 'William Anderson',
+    icon: makeMyTrip,
+    color: '#a0efaa'
+  },
+  {
+    start: 'dyndatetime(y,m,d+23,12)',
+    end: 'dyndatetime(y,m,d+26,12)',
+    title: 'Susan Taylor',
+    icon: makeMyTrip,
+    color: '#a0efaa'
+  },
+  {
+    start: 'dyndatetime(y,m,d+6,12)',
+    end: 'dyndatetime(y,m,d+9,12)',
+    title: 'Richard Jackson',
+    icon: airBnB,
+    color: '#fffa61'
+  }
+]
 
 const myView: MbscEventcalendarView = {
-  timeline: {
-    type: 'week'
+  calendar: {
+    type: 'month',
+    eventDisplay: 'exact'
   }
 }
 
-onMounted(() => {
-  getJson(
-    'https://trial.mobiscroll.com/timeline-events/',
-    (events: MbscCalendarEvent[]) => {
-      myEvents.value = events
-    },
-    'jsonp'
-  )
-})
+function handleDefaultEvent(args: MbscNewEventData) {
+  var startDateAndTime = new Date(args.start.setHours(12))
+  var endDateAndTime = new Date(args.start.setDate(args.start.getDate() + 1)).setHours(12)
+  return {
+    title: 'New reservation',
+    start: startDateAndTime,
+    end: endDateAndTime
+  }
+}
 </script>
 
 <template>
-  <!-- dragOptions -->
-  <MbscEventcalendar :view="myView" :data="myEvents" :resources="myResources" />
+  <MbscEventcalendar
+    cssClass="mds-booking-calendar"
+    :view="myView"
+    :data="myEvents"
+    :clickToCreate="true"
+    :dragToCreate="true"
+    :dragToMove="true"
+    :dragToResize="true"
+    :eventOverlap="false"
+    :extendDefaultEvent="handleDefaultEvent"
+  >
+    <template #labelContent="event">
+      <div class="mbsc-flex mds-booking-item mbsc-align-items-center">
+        <template v-if="event.original.icon">
+          <img class="mds-booking-icon" :src="event.original.icon" />
+        </template>
+        <span>{{ event.title }}</span>
+      </div>
+    </template>
+  </MbscEventcalendar>
 </template>
 
-<style></style>
+<style>
+.mds-booking-calendar .mbsc-calendar-text {
+  height: 30px !important;
+}
+
+.mds-booking-item {
+  font-size: 16px;
+  height: 30px;
+}
+
+.mds-booking-icon {
+  margin-right: 8px;
+  width: 24px;
+  height: 24px;
+}
+</style>
