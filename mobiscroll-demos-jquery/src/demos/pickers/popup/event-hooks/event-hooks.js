59436f27637255157f21984137a785a19a82e7a2
Lehel <deaklehels@gmail.com>
2024-08-21 14:19:48 +0300

    event hooks, themes static demo added

--- mobiscroll-demos-jquery/src/demos/pickers/popup/event-hooks/event-hooks.js
+++ mobiscroll-demos-jquery/src/demos/pickers/popup/event-hooks/event-hooks.js
@@ -10,10 +10,60 @@ export default {
     });
 
     $(function () {
-      // todo
+      var popup = $('#demo-popup-hooks')
+        .mobiscroll()
+        .popup({
+          display: 'anchored',
+          anchor: $('#popup-events-show-btn')[0],
+          showOverlay: false,
+          buttons: [
+            {
+              text: 'Ok',
+              handler: 'set',
+            },
+            'cancel',
+          ],
+          onClose: function (event, inst) {
+            // Your custom event handler goes here
+          },
+          onDestroy: function (event, inst) {
+            // Your custom event handler goes here
+          },
+          onInit: function (event, inst) {
+            // Logic running on component init
+          },
+          onOpen: function (event, inst) {
+            // Your custom event handler goes here
+          },
+          onPosition: function (event, inst) {
+            // Logic for component positioning
+          },
+        })
+        .mobiscroll('getInst');
+
+      popup.open();
+
+      $('#popup-events-show-btn').on('click', function () {
+        popup.open();
+        return false;
+      });
     });
   },
   // eslint-disable-next-line es5/no-template-literals
   markup: `
+<div mbsc-form>
+  <div class="mbsc-form-group">
+    <div class="mbsc-button-group-block">
+      <button mbsc-button id="popup-events-show-btn">Show popup</button>
+    </div>
+  </div>
+</div>
+<div id="demo-popup-hooks">
+  <div class="mbsc-align-center mbsc-padding">
+    <img src="https://img.mobiscroll.com/demos/logo-noshadow.jpg">
+    <h4>Welcome on our website!</h4>
+    <p>Have fun navigating through the demos.</p>
+  </div>
+</div>
   `,
 };
--- mobiscroll-demos-jquery/src/demos/pickers/popup/themes-ios-material-windows/themes-ios-material-windows.js
+++ mobiscroll-demos-jquery/src/demos/pickers/popup/themes-ios-material-windows/themes-ios-material-windows.js
@@ -9,11 +9,35 @@ export default {
     });
 
     $(function () {
-      // todo
+      var popup = $('#demo-theming-popup')
+        .mobiscroll()
+        .popup({
+          theme: 'material', // can be 'ios', 'material', 'windows' or 'auto' - in case of 'auto', the theme will automatically be set based on the platform
+          themeVariant: 'dark', // can be 'light', 'dark' or 'auto' - in case of 'auto' it is set based in the active system theme
+          display: 'anchored',
+          anchor: $('#popup-themes-show-btn')[0],
+          buttons: ['set', 'cancel'],
+        })
+        .mobiscroll('getInst');
+
+      popup.open();
+
+      $('#popup-themes-show-btn').on('click', function () {
+        popup.open();
+        return false;
+      });
     });
   },
   // eslint-disable-next-line es5/no-template-literals
   markup: `
-
+<div class="mbsc-button-group-block">
+  <button class="static-form-color-elm" id="popup-themes-show-btn" mbsc-button>Show</button>
+</div>
+<div id="demo-theming-popup">
+  <div class="mbsc-align-center mbsc-padding">
+    <h3 class="md-text-center">Hi again!</h3>
+    <p class="md-text-center">This is a popup with three custom buttons</p>
+  </div>
+</div>
   `,
 };
