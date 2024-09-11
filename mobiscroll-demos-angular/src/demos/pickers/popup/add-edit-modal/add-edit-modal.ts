52a70a9e1f7210030b42325a9869be4bcdcf85a5
Lehel <deaklehels@gmail.com>
2024-09-11 10:15:50 +0300

    add event demo refactored

--- mobiscroll-demos-angular/src/demos/pickers/popup/add-edit-modal/add-edit-modal.ts
+++ mobiscroll-demos-angular/src/demos/pickers/popup/add-edit-modal/add-edit-modal.ts
@@ -1,5 +1,14 @@
 import { Component, ViewChild, ViewEncapsulation } from '@angular/core';
-import { MbscDatepickerOptions, MbscPopup, MbscPopupOptions, setOptions /* localeImport */ } from '@mobiscroll/angular';
+import {
+  MbscCalendarEvent,
+  MbscDatepickerOptions,
+  MbscEventcalendarView,
+  MbscPopup,
+  MbscPopupOptions,
+  Notifications,
+  setOptions /* localeImport */,
+} from '@mobiscroll/angular';
+import { dyndatetime } from '../../../../app/app.util';
 
 setOptions({
   // locale,
@@ -10,22 +19,70 @@ setOptions({
   selector: 'app-popup-add-edit-modal',
   encapsulation: ViewEncapsulation.None,
   templateUrl: './add-edit-modal.html',
+  providers: [Notifications],
 })
 export class AppComponent {
+  constructor(private notify: Notifications) {}
+
   @ViewChild('popup', { static: false }) popup!: MbscPopup;
 
-  colorPicker: any;
+  myEvents: MbscCalendarEvent[] = [
+    {
+      id: 1,
+      start: dyndatetime('y, m, 8, 13'),
+      end: dyndatetime('y, m, 8, 13, 45'),
+      title: "Lunch @ Butcher's",
+      description: '',
+      allDay: false,
+      bufferBefore: 15,
+      free: true,
+      color: '#009788',
+    },
+    {
+      id: 2,
+      start: dyndatetime('y, m, d, 15'),
+      end: dyndatetime('y, m, d, 16'),
+      title: 'Conference',
+      description: '',
+      allDay: false,
+      bufferBefore: 30,
+      free: false,
+      color: '#ff9900',
+    },
+    {
+      id: 3,
+      start: dyndatetime('y, m, d - 1, 18'),
+      end: dyndatetime('y, m, d - 1, 22'),
+      title: 'Site Visit',
+      description: '',
+      allDay: false,
+      bufferBefore: 60,
+      free: true,
+      color: '#3f51b5',
+    },
+    {
+      id: 4,
+      start: dyndatetime('y, m, d + 1, 10, 30'),
+      end: dyndatetime('y, m, d + 1, 11, 30'),
+      title: 'Stakeholder mtg.',
+      description: '',
+      allDay: false,
+      free: false,
+      color: '#f44437',
+    },
+  ];
+
+  myView: MbscEventcalendarView = { calendar: { type: 'month' } };
+
   popupEventTitle: string | undefined;
   popupEventDescription = '';
   popupEventAllDay = true;
-  popupTravelTime = 0;
   popupEventDates: any;
-  popupEventStatus = 'busy';
-  calendarSelectedDate: any = new Date();
-  switchLabel: any = 'All-day';
+  popupEventStatus = false;
+  mySelectedDate: any;
 
-  datePickerControls = ['date'];
-  datetimePickerControls = ['datetime'];
+  datePickerControls = ['calendar'];
+  datetimePickerControls = ['calendar', 'time'];
 
   datePickerOptions: MbscDatepickerOptions = {
     select: 'range',
@@ -43,14 +100,28 @@ export class AppComponent {
       'cancel',
       {
         text: 'Add',
-        // keyCode: 'enter',
+        keyCode: 'enter',
         handler: () => {
-          this.popup.close();
+          const newEvent = {
+            title: this.popupEventTitle,
+            description: this.popupEventDescription,
+            allDay: this.popupEventAllDay,
+            status: this.popupEventStatus,
+            start: this.popupEventDates[0],
+            end: this.popupEventDates[1],
+          };
 
-          // how to add?? toast // notification? why not works
-          // mobiscroll.toast({
-          //   message: 'Event added',
-          // });
+          this.myEvents = [...this.myEvents, newEvent];
+          this.mySelectedDate = this.popupEventDates[0];
+
+          // navigate the calendar
+          // this.calendarSelectedDate = this.popupEventDates[0];
+          // setSelectedDate(newEvent.start);
+
+          this.popup.close();
+          this.notify.toast({
+            message: 'Event added',
+          });
         },
         cssClass: 'mbsc-popup-button-primary',
       },
@@ -59,6 +130,11 @@ export class AppComponent {
   };
 
   addEvent(): void {
+    this.popupEventTitle = 'New Event';
+    this.popupEventDescription = '';
+    this.popupEventAllDay = false;
+    this.popupEventStatus = false;
+    this.popupEventDates = [new Date(), new Date()];
     this.popup.open();
   }
 }
