152d5d62b9da0e4c6ffcd504f4921d6fd6f871e7
szily20 <szily20@gmail.com>
2024-09-13 13:49:53 +0300

    Updated resource reorder demo

--- mobiscroll-demos-jquery/src/demos/eventcalendar/timeline/resource-drag-drop-reorder/resource-drag-drop-reorder.js
+++ mobiscroll-demos-jquery/src/demos/eventcalendar/timeline/resource-drag-drop-reorder/resource-drag-drop-reorder.js
@@ -10,13 +10,6 @@ export default {
     });
 
     $(function () {
-      var timelineView = {
-        type: 'week',
-        size: 4,
-        resolutionHorizontal: 'day',
-        eventList: false,
-        resourceOrder: false,
-      };
       var resources = [
         { id: 1, name: 'Resource 1' },
         { id: 2, name: 'Resource 2 (Reorder disabled)', reorder: false },
@@ -26,88 +19,116 @@ export default {
           id: 5,
           name: 'Group 1',
           children: [
-            { id: '1-1', name: 'Resource 5' },
-            { id: '1-2', name: 'Resource 6' },
-            { id: '1-3', name: 'Resource 7' },
+            { id: 6, name: 'Resource 5' },
+            { id: 7, name: 'Resource 6' },
+            { id: 8, name: 'Resource 7' },
           ],
         },
-        { id: 6, name: 'Resource 8' },
-        { id: 7, name: 'Resource 9' },
-        {
-          id: 8,
-          name: 'Group 2 (Reorder disabled)',
-          reorder: false,
-          children: [
-            { id: '2-1', name: 'Resource 10' },
-            { id: '2-2', name: 'Resource 11' },
-            { id: '2-3', name: 'Resource 12' },
-          ],
-        },
-        { id: 9, name: 'Resource 13' },
-        { id: 10, name: 'Resource 14' },
+        { id: 9, name: 'Resource 8' },
+        { id: 10, name: 'Resource 9' },
         {
           id: 11,
+          name: 'Group 2 (Dropping not allowed)',
+          children: [
+            { id: 12, name: 'Resource 10' },
+            { id: 13, name: 'Resource 11' },
+            { id: 14, name: 'Resource 12' },
+          ],
+        },
+        { id: 15, name: 'Resource 13' },
+        { id: 16, name: 'Resource 14' },
+        {
+          id: 17,
           name: 'Group 3',
           children: [
-            { id: '3-1', name: 'Resource 15' },
-            { id: '3-2', name: 'Resource 16' },
-            { id: '3-3', name: 'Resource 17' },
+            { id: 18, name: 'Resource 15' },
+            { id: 19, name: 'Resource 16' },
+            { id: 20, name: 'Resource 17' },
           ],
         },
-        { id: 12, name: 'Resource 18' },
+        { id: 21, name: 'Resource 18' },
         {
-          id: 13,
-          name: 'Group 4 (Dropping not allowed)',
+          id: 22,
+          name: 'Group 4 (Reorder disabled)',
+          reorder: false,
           children: [
-            { id: '4-1', name: 'Resource 19' },
-            { id: '4-2', name: 'Resource 20' },
-            { id: '4-3', name: 'Resource 21' },
+            { id: 23, name: 'Resource 19' },
+            { id: 24, name: 'Resource 20' },
+            { id: 25, name: 'Resource 21' },
           ],
         },
-        { id: 14, name: 'Resource 22' },
+        { id: 26, name: 'Resource 22' },
       ];
+
       var tempResources = [];
       var calInst = $('#demo-resource-drag-drop-reorder')
         .mobiscroll()
         .eventcalendar({
-          refDate: new Date(2024, 8, 1),
+          cssClass: 'mds-resource-drag-drop-reorder',
           selectedDate: new Date(2024, 8, 1),
           view: {
-            timeline: timelineView,
+            timeline: {
+              type: 'month',
+              resolutionHorizontal: 'day',
+              resourceOrder: false,
+            },
           },
           data: [
-            { title: 'Event 1', start: '2024-09-01', end: '2024-09-02', resource: 14, color: 'green' },
-            { title: 'Event 2', start: '2024-09-03', end: '2024-09-04', resource: 2, color: 'blue' },
-            { title: 'Event 3', start: '2024-09-05', end: '2024-09-06', resource: 10, color: 'red' },
-            { title: 'Event 4', start: '2024-09-07', end: '2024-09-08', resource: 9, color: 'yellow' },
-            { title: 'Event 5', start: '2024-09-09', end: '2024-09-10', resource: 6, color: 'purple' },
-            { title: 'Event 6', start: '2024-09-11', end: '2024-09-12', resource: 1, color: 'orange' },
-            { title: 'Event 7', start: '2024-09-13', end: '2024-09-14', resource: 4, color: 'pink' },
-            { title: 'Event 8', start: '2024-09-15', end: '2024-09-16', resource: 3, color: 'cyan' },
-            { title: 'Event 9', start: '2024-09-17', end: '2024-09-18', resource: 12, color: 'magenta' },
-            { title: 'Event 10', start: '2024-09-19', end: '2024-09-20', resource: 7, color: 'lime' },
-            { title: 'Event 1 - 1', start: '2024-09-01', end: '2024-09-02', resource: '3-3', color: 'green' },
-            { title: 'Event 1 - 2', start: '2024-09-03', end: '2024-09-04', resource: '3-2', color: 'blue' },
-            { title: 'Event 1 - 3', start: '2024-09-05', end: '2024-09-06', resource: '1-3', color: 'red' },
-            { title: 'Event 2 - 1', start: '2024-09-07', end: '2024-09-08', resource: '2-1', color: 'yellow' },
-            { title: 'Event 2 - 2', start: '2024-09-09', end: '2024-09-10', resource: '4-2', color: 'purple' },
-            { title: 'Event 2 - 3', start: '2024-09-11', end: '2024-09-12', resource: '2-3', color: 'orange' },
-            { title: 'Event 3 - 1', start: '2024-09-13', end: '2024-09-14', resource: '3-1', color: 'pink' },
-            { title: 'Event 3 - 2', start: '2024-09-15', end: '2024-09-16', resource: '3-2', color: 'cyan' },
-            { title: 'Event 3 - 3', start: '2024-09-17', end: '2024-09-18', resource: '2-1', color: 'magenta' },
-            { title: 'Event 4 - 1', start: '2024-09-19', end: '2024-09-20', resource: '2-1', color: 'lime' },
-            { title: 'Event 4 - 2', start: '2024-09-21', end: '2024-09-22', resource: '4-2', color: 'teal' },
-            { title: 'Event 4 - 3', start: '2024-09-23', end: '2024-09-24', resource: '1-3', color: 'violet' },
+            { title: 'Event 1', start: 'dyndatetime(y,m,1)', end: 'dyndatetime(y,m,5)', resource: 14, color: 'green' },
+            { title: 'Event 2', start: 'dyndatetime(y,m,3)', end: 'dyndatetime(y,m,7)', resource: 2, color: 'blue' },
+            { title: 'Event 3', start: 'dyndatetime(y,m,5)', end: 'dyndatetime(y,m,10)', resource: 10, color: 'red' },
+            { title: 'Event 4', start: 'dyndatetime(y,m,7)', end: 'dyndatetime(y,m,11)', resource: 9, color: 'yellow' },
+            { title: 'Event 5', start: 'dyndatetime(y,m,9)', end: 'dyndatetime(y,m,12)', resource: 6, color: 'purple' },
+            //<hidden>
+            { title: 'Event 6', start: 'dyndatetime(y,m,11)', end: 'dyndatetime(y,m,15)', resource: 1, color: 'orange' },
+            { title: 'Event 7', start: 'dyndatetime(y,m,13)', end: 'dyndatetime(y,m,17)', resource: 4, color: 'pink' },
+            { title: 'Event 8', start: 'dyndatetime(y,m,15)', end: 'dyndatetime(y,m,19)', resource: 3, color: 'cyan' },
+            { title: 'Event 9', start: 'dyndatetime(y,m,17)', end: 'dyndatetime(y,m,21)', resource: 12, color: 'magenta' },
+            { title: 'Event 10', start: 'dyndatetime(y,m,19)', end: 'dyndatetime(y,m,24)', resource: 7, color: 'lime' },
+            { title: 'Event 11', start: 'dyndatetime(y,m,21)', end: 'dyndatetime(y,m,25)', resource: 14, color: 'green' },
+            { title: 'Event 12', start: 'dyndatetime(y,m,23)', end: 'dyndatetime(y,m,28)', resource: 2, color: 'blue' },
+            { title: 'Event 13', start: 'dyndatetime(y,m,25)', end: 'dyndatetime(y,m,29)', resource: 10, color: 'red' },
+            { title: 'Event 14', start: 'dyndatetime(y,m,27)', end: 'dyndatetime(y,m,30)', resource: 9, color: 'yellow' },
+            { title: 'Event 15', start: 'dyndatetime(y,m,29)', end: 'dyndatetime(y,m,3)', resource: 6, color: 'purple' },
+            { title: 'Event 16', start: 'dyndatetime(y,m,1)', end: 'dyndatetime(y,m,5)', resource: 1, color: 'orange' },
+            { title: 'Event 17', start: 'dyndatetime(y,m,20)', end: 'dyndatetime(y,m,25)', resource: 4, color: 'pink' },
+            { title: 'Event 18', start: 'dyndatetime(y,m,5)', end: 'dyndatetime(y,m,9)', resource: 3, color: 'cyan' },
+            { title: 'Event 19', start: 'dyndatetime(y,m,7)', end: 'dyndatetime(y,m,11)', resource: 12, color: 'magenta' },
+            { title: 'Event 20', start: 'dyndatetime(y,m,9)', end: 'dyndatetime(y,m,13)', resource: 7, color: 'lime' },
+            { title: 'Event 21', start: 'dyndatetime(y,m,11)', end: 'dyndatetime(y,m,15)', resource: 14, color: 'teal' },
+            { title: 'Event 22', start: 'dyndatetime(y,m,19)', end: 'dyndatetime(y,m,27)', resource: 6, color: 'violet' },
+            { title: 'Event 23', start: 'dyndatetime(y,m,15)', end: 'dyndatetime(y,m,19)', resource: 10, color: 'blue' },
+            { title: 'Event 24', start: 'dyndatetime(y,m,17)', end: 'dyndatetime(y,m,21)', resource: 3, color: 'cyan' },
+            { title: 'Event 25', start: 'dyndatetime(y,m,19)', end: 'dyndatetime(y,m,23)', resource: 1, color: 'orange' },
+            { title: 'Event 26', start: 'dyndatetime(y,m,21)', end: 'dyndatetime(y,m,25)', resource: 2, color: 'green' },
+            { title: 'Event 27', start: 'dyndatetime(y,m,23)', end: 'dyndatetime(y,m,27)', resource: 9, color: 'yellow' },
+            { title: 'Event 28', start: 'dyndatetime(y,m,23)', end: 'dyndatetime(y,m,30)', resource: 4, color: 'pink' },
+            { title: 'Event 29', start: 'dyndatetime(y,m,27)', end: 'dyndatetime(y,m,31)', resource: 12, color: 'magenta' },
+            { title: 'Event 30', start: 'dyndatetime(y,m,12)', end: 'dyndatetime(y,m,18)', resource: 7, color: 'lime' },
+            { title: 'Event 31', start: 'dyndatetime(y,m,2)', end: 'dyndatetime(y,m,7)', resource: 14, color: 'green' },
+            { title: 'Event 32', start: 'dyndatetime(y,m,4)', end: 'dyndatetime(y,m,10)', resource: 15, color: 'blue' },
+            { title: 'Event 33', start: 'dyndatetime(y,m,3)', end: 'dyndatetime(y,m,8)', resource: 16, color: 'red' },
+            { title: 'Event 34', start: 'dyndatetime(y,m,7)', end: 'dyndatetime(y,m,13)', resource: 17, color: 'yellow' },
+            { title: 'Event 35', start: 'dyndatetime(y,m,18)', end: 'dyndatetime(y,m,26)', resource: 18, color: 'purple' },
+            { title: 'Event 36', start: 'dyndatetime(y,m,9)', end: 'dyndatetime(y,m,14)', resource: 19, color: 'orange' },
+            { title: 'Event 37', start: 'dyndatetime(y,m,8)', end: 'dyndatetime(y,m,16)', resource: 20, color: 'pink' },
+            { title: 'Event 38', start: 'dyndatetime(y,m,11)', end: 'dyndatetime(y,m,19)', resource: 21, color: 'cyan' },
+            { title: 'Event 39', start: 'dyndatetime(y,m,7)', end: 'dyndatetime(y,m,11)', resource: 22, color: 'magenta' },
+            { title: 'Event 40', start: 'dyndatetime(y,m,6)', end: 'dyndatetime(y,m,15)', resource: 23, color: 'lime' },
+            { title: 'Event 41', start: 'dyndatetime(y,m,13)', end: 'dyndatetime(y,m,21)', resource: 24, color: 'teal' },
+            { title: 'Event 42', start: 'dyndatetime(y,m,22)', end: 'dyndatetime(y,m,29)', resource: 25, color: 'violet' },
+            { title: 'Event 43', start: 'dyndatetime(y,m,1)', end: 'dyndatetime(y,m,9)', resource: 26, color: 'brown' },
+            //</hidden>
           ],
           resources: resources,
           onResourceOrderUpdate: function (args) {
             tempResources = args.resources;
-            if (args.parent && args.parent.id === 13) {
+            if (args.parent && args.parent.id === 11) {
               mobiscroll.toast({
                 //<hidden>
                 // theme,//</hidden>
                 // context,
-                message: 'Drop to Group 4 is not allowed!',
+                message: 'Drop to Group 2 is not allowed!',
               });
               return false;
             }
@@ -115,12 +136,11 @@ export default {
           renderHeader: function () {
             return (
               '<div mbsc-calendar-nav class="mds-header-filter-nav"></div>' +
-              '<div class="mds-header-filter mbsc-flex-1-0">' +
-              'Reorder:' +
-              '<button mbsc-button class="mds-reorder-switch mds-enable">Enable</buttom>' +
-              '<button mbsc-button class="mds-reorder-save mds-update mds-hidden">Save</buttom>' +
-              '<button mbsc-button class="mds-reorder-cancel mds-update mds-hidden">Cancel</buttom>' +
-              '</div>' +
+              '<div class="mds-header-filter mbsc-flex mbsc-flex-1-1">' +
+              '<button mbsc-button data-start-icon="material-list" data-variant="flat" data-color="dark" class="mds-reorder-switch mds-enable">Reorder resources</button>' +
+              '<button mbsc-button class="mds-reorder-save mds-update mds-hidden">Save resource order</button>' +
+              '<button mbsc-button class="mds-reorder-cancel mds-update mds-hidden">Cancel</button>' +
+              '<div class="mds-header-filter-separator"></div>' +
               '<button mbsc-calendar-prev class="mds-header-filter-prev"></button>' +
               '<button mbsc-calendar-today class="mds-header-filter-today"></button>' +
               '<button mbsc-calendar-next class="mds-header-filter-next"></button>'
@@ -146,9 +166,7 @@ export default {
         calInst.setOptions({
           view: {
             timeline: {
-              type: 'week',
-              size: 4,
-              resolutionHorizontal: 'day',
+              type: 'month',
               resourceOrder: true,
             },
           },
@@ -161,12 +179,17 @@ export default {
           resources: resources.slice(),
           view: {
             timeline: {
-              type: 'week',
-              size: 4,
-              resolutionHorizontal: 'day',
+              type: 'month',
               resourceOrder: false,
             },
           },
+        });
+
+        mobiscroll.toast({
+          //<hidden>
+          // theme,//</hidden>
+          // context,
+          message: 'Resource order canceled',
         });
       });
 
@@ -179,12 +202,17 @@ export default {
           resources: resources.slice(),
           view: {
             timeline: {
-              type: 'week',
-              size: 4,
-              resolutionHorizontal: 'day',
+              type: 'month',
               resourceOrder: false,
             },
           },
+        });
+
+        mobiscroll.toast({
+          //<hidden>
+          // theme,//</hidden>
+          // context,
+          message: 'Resource order saved',
         });
       });
     });
@@ -195,6 +223,25 @@ export default {
 `,
   // eslint-disable-next-line es5/no-template-literals
   css: `
+.mds-resource-drag-drop-reorder .mbsc-timeline-resource-col {
+  width: 230px;
+}
+
+.mds-header-filter {
+  justify-content: end;
+  align-items: center;
+}
+
+.mds-header-filter-separator {
+  display: flex;
+
+  align-self: center;
+  height: 25px;
+  width: 1px;
+  margin: 0 0.25em 0 .5em;
+  background: currentColor;
+}
+
 .mds-hidden {
   display: none;
 }
