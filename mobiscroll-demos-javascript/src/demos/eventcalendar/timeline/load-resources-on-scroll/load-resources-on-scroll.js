a8849dd1a2c9866ba018a39048e17978ffaa1011
szily20 <szily20@gmail.com>
2024-09-20 14:43:53 +0300

    Added load resoruce on scroll demo

---
+++ mobiscroll-demos-javascript/src/demos/eventcalendar/timeline/load-resources-on-scroll/load-resources-on-scroll.js
@@ -0,0 +1,26 @@
+import * as mobiscroll from '@mobiscroll/javascript';
+
+export default {
+  // eslint-disable-next-line es5/no-shorthand-properties
+  init() {
+    mobiscroll.setOptions({
+      // locale,
+      // theme
+    });
+
+    mobiscroll.eventcalendar('#demo-load-resources-on-scroll', {
+      view: {
+        timeline: {
+          type: 'week',
+        },
+      },
+
+    });
+  },
+  // eslint-disable-next-line es5/no-template-literals
+  markup: `
+<div id="demo-load-resources-on-scroll"></div>
+`,
+  // eslint-disable-next-line es5/no-template-literals
+  css: ``, 
+};
