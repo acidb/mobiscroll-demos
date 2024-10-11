e8c7e0324c0fae6227b0dd53cce5718fa43bea46
Lehel <deaklehels@gmail.com>
2024-10-11 10:28:53 +0300

    copy script demos sync

--- copy.js
+++ copy.js
@@ -1,5 +1,6 @@
-const fse = require('fs-extra');
-const config = require('./config.json');
+const { execSync } = require("child_process");
+const fse = require("fs-extra");
+const config = require("./config.json");
 
 const filter = (src) => {
   return !/node_modules|dist|\.angular/.test(src);
@@ -7,10 +8,57 @@ const filter = (src) => {
 
 const opt = { filter };
 
-fse.copy('mobiscroll-demos-angular', config.demosPathPublic + '/mobiscroll-demos-angular', opt);
-fse.copy('mobiscroll-demos-javascript', config.demosPathPublic + '/mobiscroll-demos-javascript', opt);
-fse.copy('mobiscroll-demos-jquery', config.demosPathPublic + '/mobiscroll-demos-jquery', opt);
-fse.copy('mobiscroll-demos-react', config.demosPathPublic + '/mobiscroll-demos-react', opt);
-fse.copy('mobiscroll-demos-react-ts', config.demosPathPublic + '/mobiscroll-demos-react-ts', opt);
-fse.copy('mobiscroll-demos-vue', config.demosPathPublic + '/mobiscroll-demos-vue', opt);
-fse.copy('mobiscroll-demos-vue-ts', config.demosPathPublic + '/mobiscroll-demos-vue-ts', opt);
+fse.copySync(
+  "mobiscroll-demos-javascript",
+  config.demosPathPublic + "/mobiscroll-demos-javascript",
+  opt
+);
+fse.copySync(
+  "mobiscroll-demos-angular",
+  config.demosPathPublic + "/mobiscroll-demos-angular",
+  opt
+);
+fse.copySync(
+  "mobiscroll-demos-jquery",
+  config.demosPathPublic + "/mobiscroll-demos-jquery",
+  opt
+);
+fse.copySync(
+  "mobiscroll-demos-react",
+  config.demosPathPublic + "/mobiscroll-demos-react",
+  opt
+);
+fse.copySync(
+  "mobiscroll-demos-react-ts",
+  config.demosPathPublic + "/mobiscroll-demos-react-ts",
+  opt
+);
+fse.copySync(
+  "mobiscroll-demos-vue",
+  config.demosPathPublic + "/mobiscroll-demos-vue",
+  opt
+);
+fse.copySync(
+  "mobiscroll-demos-vue-ts",
+  config.demosPathPublic + "/mobiscroll-demos-vue-ts",
+  opt
+);
+
+const run = (cmd, repo) => execSync(cmd, { cwd: repo, stdio: "inherit" });
+const sync = (repo) => {
+  run("git add .", repo);
+  run('git commit -m "sync"', repo);
+  run("git push origin main", repo);
+};
+
+[
+  "mobiscroll-demos-angular",
+  "mobiscroll-demos-javascript",
+  "mobiscroll-demos-jquery",
+  "mobiscroll-demos-react",
+  "mobiscroll-demos-react-ts",
+  "mobiscroll-demos-vue",
+  "mobiscroll-demos-vue-ts",
+].forEach((repo) => sync(`${config.demosPathPublic}/${repo}`));
+
+console.log("Done!");
