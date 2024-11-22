be3015709d60633ae4bade33abb3383b18088885
Lehel <deaklehels@gmail.com>
2024-11-22 12:58:57 +0200

    routes and files created for frameworks

---
+++ mobiscroll-demos-javascript/src/demos/eventcalendar/timeline/custom-zoom-levels/custom-zoom-levels.js
@@ -0,0 +1,26 @@
+import * as mobiscroll from '@mobiscroll/javascript';
+
+export default {
+  // eslint-disable-next-line es5/no-shorthand-properties
+  init() {
+    mobiscroll.setOptions({
+      // locale,
+      // theme
+    });
+
+    mobiscroll.eventcalendar('#demo-custom-zoom-levels', {
+      view: {
+        timeline: {
+          type: 'week',
+        },
+      },
+
+    });
+  },
+  // eslint-disable-next-line es5/no-template-literals
+  markup: `
+<div id="demo-custom-zoom-levels"></div>
+`,
+  // eslint-disable-next-line es5/no-template-literals
+  css: ``, 
+};
