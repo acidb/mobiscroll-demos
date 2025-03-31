36d6addcb565684117df1da1ef9a3fbdbf267907
Halmen Istvan <isti@mobiscroll.com>
2025-03-31 18:35:57 +0300

    Control the column width demo review

--- mobiscroll-demos-jquery/src/demos/eventcalendar/scheduler/control-the-column-width/control-the-column-width.js
+++ mobiscroll-demos-jquery/src/demos/eventcalendar/scheduler/control-the-column-width/control-the-column-width.js
@@ -10,8 +10,6 @@ export default {
     });
 
     $(function () {
-      var myCustomClass = 'my-col-width-small';
-
       var myResources = [
         { id: 1, name: 'Bart', color: '#328E39' },
         { id: 2, name: 'Jake', color: '#00AABB' },
@@ -22,48 +20,48 @@ export default {
       ];
 
       var myResEvents = [
-        { resource: 1, start: 'dyndatetime(y,m,d,9)', end: 'dyndatetime(y,m,d,12)', title: 'Meeting' },
-        { resource: 2, start: 'dyndatetime(y,m,d,11)', end: 'dyndatetime(y,m,d,13)', title: 'Call' },
-        { resource: 3, start: 'dyndatetime(y,m,d,14)', end: 'dyndatetime(y,m,d,16)', title: 'Review' },
-        { resource: 4, start: 'dyndatetime(y,m,d,10)', end: 'dyndatetime(y,m,d,13)', title: 'Session' },
-        { resource: 5, start: 'dyndatetime(y,m,d,15)', end: 'dyndatetime(y,m,d,17)', title: 'Sprint' },
-        { resource: 6, start: 'dyndatetime(y,m,d,8)', end: 'dyndatetime(y,m,d,10)', title: 'Stand-up' },
-        { resource: 1, start: 'dyndatetime(y,m,d-1,9)', end: 'dyndatetime(y,m,d-1,12)', title: 'Planning' },
-        { resource: 2, start: 'dyndatetime(y,m,d-1,13)', end: 'dyndatetime(y,m,d-1,15)', title: 'Discussion' },
-        { resource: 3, start: 'dyndatetime(y,m,d-1,10)', end: 'dyndatetime(y,m,d-1,12)', title: 'Review' },
-        { resource: 4, start: 'dyndatetime(y,m,d-1,14)', end: 'dyndatetime(y,m,d-1,16)', title: 'Feedback' },
-        { resource: 5, start: 'dyndatetime(y,m,d-1,11)', end: 'dyndatetime(y,m,d-1,13)', title: 'Design' },
-        { resource: 6, start: 'dyndatetime(y,m,d-1,9)', end: 'dyndatetime(y,m,d-1,11)', title: 'Meeting' },
-        { resource: 1, start: 'dyndatetime(y,m,d-2,8)', end: 'dyndatetime(y,m,d-2,10)', title: 'Kickoff' },
-        { resource: 2, start: 'dyndatetime(y,m,d-2,15)', end: 'dyndatetime(y,m,d-2,17)', title: 'Talk' },
-        { resource: 3, start: 'dyndatetime(y,m,d-2,11)', end: 'dyndatetime(y,m,d-2,13)', title: 'Demo' },
-        { resource: 4, start: 'dyndatetime(y,m,d-2,14)', end: 'dyndatetime(y,m,d-2,16)', title: 'Retro' },
-        { resource: 5, start: 'dyndatetime(y,m,d-2,10)', end: 'dyndatetime(y,m,d-2,12)', title: 'Review' },
-        { resource: 6, start: 'dyndatetime(y,m,d-2,13)', end: 'dyndatetime(y,m,d-2,15)', title: 'Update' },
-        { resource: 1, start: 'dyndatetime(y,m,d-3,7)', end: 'dyndatetime(y,m,d-3,9)', title: 'Sync' },
-        { resource: 2, start: 'dyndatetime(y,m,d-3,16)', end: 'dyndatetime(y,m,d-3,18)', title: 'Check-in' },
-        { resource: 3, start: 'dyndatetime(y,m,d-3,9)', end: 'dyndatetime(y,m,d-3,11)', title: 'Planning' },
-        { resource: 4, start: 'dyndatetime(y,m,d-3,12)', end: 'dyndatetime(y,m,d-3,14)', title: 'Collab' },
-        { resource: 5, start: 'dyndatetime(y,m,d-3,11)', end: 'dyndatetime(y,m,d-3,13)', title: 'Stand-up' },
-        { resource: 6, start: 'dyndatetime(y,m,d-3,14)', end: 'dyndatetime(y,m,d-3,16)', title: 'Retro' },
-        { resource: 1, start: 'dyndatetime(y,m,d+1,9)', end: 'dyndatetime(y,m,d+1,12)', title: 'Review' },
-        { resource: 2, start: 'dyndatetime(y,m,d+1,11)', end: 'dyndatetime(y,m,d+1,13)', title: 'Integration' },
-        { resource: 3, start: 'dyndatetime(y,m,d+1,15)', end: 'dyndatetime(y,m,d+1,17)', title: 'Retrospective' },
-        { resource: 4, start: 'dyndatetime(y,m,d+1,10)', end: 'dyndatetime(y,m,d+1,12)', title: 'Testing' },
-        { resource: 5, start: 'dyndatetime(y,m,d+1,16)', end: 'dyndatetime(y,m,d+1,18)', title: 'Demo' },
-        { resource: 6, start: 'dyndatetime(y,m,d+1,14)', end: 'dyndatetime(y,m,d+1,16)', title: 'Call' },
-        { resource: 1, start: 'dyndatetime(y,m,d+2,8)', end: 'dyndatetime(y,m,d+2,10)', title: 'Kickoff' },
-        { resource: 2, start: 'dyndatetime(y,m,d+2,13)', end: 'dyndatetime(y,m,d+2,15)', title: 'Demo' },
-        { resource: 3, start: 'dyndatetime(y,m,d+2,11)', end: 'dyndatetime(y,m,d+2,13)', title: 'Review' },
-        { resource: 4, start: 'dyndatetime(y,m,d+2,10)', end: 'dyndatetime(y,m,d+2,12)', title: 'Stand-up' },
-        { resource: 5, start: 'dyndatetime(y,m,d+2,15)', end: 'dyndatetime(y,m,d+2,17)', title: 'Retro' },
-        { resource: 6, start: 'dyndatetime(y,m,d+2,9)', end: 'dyndatetime(y,m,d+2,11)', title: 'Update' },
-        { resource: 1, start: 'dyndatetime(y,m,d+3,10)', end: 'dyndatetime(y,m,d+3,12)', title: 'Planning' },
-        { resource: 2, start: 'dyndatetime(y,m,d+3,14)', end: 'dyndatetime(y,m,d+3,16)', title: 'Fixing' },
-        { resource: 3, start: 'dyndatetime(y,m,d+3,8)', end: 'dyndatetime(y,m,d+3,10)', title: 'Onboarding' },
-        { resource: 4, start: 'dyndatetime(y,m,d+3,13)', end: 'dyndatetime(y,m,d+3,15)', title: 'Rollout' },
-        { resource: 5, start: 'dyndatetime(y,m,d+3,12)', end: 'dyndatetime(y,m,d+3,14)', title: 'Review' },
-        { resource: 6, start: 'dyndatetime(y,m,d+3,16)', end: 'dyndatetime(y,m,d+3,18)', title: 'Strategy' },
+        { start: 'dyndatetime(y,m,d-3,7)', end: 'dyndatetime(y,m,d-3,9)', title: 'Sync', resource: 1 },
+        { start: 'dyndatetime(y,m,d-2,8)', end: 'dyndatetime(y,m,d-2,10)', title: 'Kickoff', resource: 1 },
+        { start: 'dyndatetime(y,m,d-1,9)', end: 'dyndatetime(y,m,d-1,12)', title: 'Planning', resource: 1 },
+        { start: 'dyndatetime(y,m,d,9)', end: 'dyndatetime(y,m,d,12)', title: 'Meeting', resource: 1 },
+        { start: 'dyndatetime(y,m,d+1,9)', end: 'dyndatetime(y,m,d+1,12)', title: 'Review', resource: 1 },
+        { start: 'dyndatetime(y,m,d+2,8)', end: 'dyndatetime(y,m,d+2,10)', title: 'Kickoff', resource: 1 },
+        { start: 'dyndatetime(y,m,d+3,10)', end: 'dyndatetime(y,m,d+3,12)', title: 'Planning', resource: 1 },
+        { start: 'dyndatetime(y,m,d-3,16)', end: 'dyndatetime(y,m,d-3,18)', title: 'Check-in', resource: 2 },
+        { start: 'dyndatetime(y,m,d-2,15)', end: 'dyndatetime(y,m,d-2,17)', title: 'Talk', resource: 2 },
+        { start: 'dyndatetime(y,m,d-1,13)', end: 'dyndatetime(y,m,d-1,15)', title: 'Discussion', resource: 2 },
+        { start: 'dyndatetime(y,m,d,11)', end: 'dyndatetime(y,m,d,13)', title: 'Call', resource: 2 },
+        { start: 'dyndatetime(y,m,d+1,11)', end: 'dyndatetime(y,m,d+1,13)', title: 'Integration', resource: 2 },
+        { start: 'dyndatetime(y,m,d+2,13)', end: 'dyndatetime(y,m,d+2,15)', title: 'Demo', resource: 2 },
+        { start: 'dyndatetime(y,m,d+3,14)', end: 'dyndatetime(y,m,d+3,16)', title: 'Fixing', resource: 2 },
+        { start: 'dyndatetime(y,m,d-3,9)', end: 'dyndatetime(y,m,d-3,11)', title: 'Planning', resource: 3 },
+        { start: 'dyndatetime(y,m,d-2,11)', end: 'dyndatetime(y,m,d-2,13)', title: 'Demo', resource: 3 },
+        { start: 'dyndatetime(y,m,d-1,10)', end: 'dyndatetime(y,m,d-1,12)', title: 'Review', resource: 3 },
+        { start: 'dyndatetime(y,m,d,14)', end: 'dyndatetime(y,m,d,16)', title: 'Review', resource: 3 },
+        { start: 'dyndatetime(y,m,d+1,15)', end: 'dyndatetime(y,m,d+1,17)', title: 'Retrospective', resource: 3 },
+        { start: 'dyndatetime(y,m,d+2,11)', end: 'dyndatetime(y,m,d+2,13)', title: 'Review', resource: 3 },
+        { start: 'dyndatetime(y,m,d+3,8)', end: 'dyndatetime(y,m,d+3,10)', title: 'Onboarding', resource: 3 },
+        { start: 'dyndatetime(y,m,d-3,12)', end: 'dyndatetime(y,m,d-3,14)', title: 'Collab', resource: 4 },
+        { start: 'dyndatetime(y,m,d-2,14)', end: 'dyndatetime(y,m,d-2,16)', title: 'Retro', resource: 4 },
+        { start: 'dyndatetime(y,m,d-1,14)', end: 'dyndatetime(y,m,d-1,16)', title: 'Feedback', resource: 4 },
+        { start: 'dyndatetime(y,m,d,10)', end: 'dyndatetime(y,m,d,13)', title: 'Session', resource: 4 },
+        { start: 'dyndatetime(y,m,d+1,10)', end: 'dyndatetime(y,m,d+1,12)', title: 'Testing', resource: 4 },
+        { start: 'dyndatetime(y,m,d+2,10)', end: 'dyndatetime(y,m,d+2,12)', title: 'Stand-up', resource: 4 },
+        { start: 'dyndatetime(y,m,d+3,13)', end: 'dyndatetime(y,m,d+3,15)', title: 'Rollout', resource: 4 },
+        { start: 'dyndatetime(y,m,d-3,11)', end: 'dyndatetime(y,m,d-3,13)', title: 'Stand-up', resource: 5 },
+        { start: 'dyndatetime(y,m,d-2,10)', end: 'dyndatetime(y,m,d-2,12)', title: 'Review', resource: 5 },
+        { start: 'dyndatetime(y,m,d-1,11)', end: 'dyndatetime(y,m,d-1,13)', title: 'Design', resource: 5 },
+        { start: 'dyndatetime(y,m,d,15)', end: 'dyndatetime(y,m,d,17)', title: 'Sprint', resource: 5 },
+        { start: 'dyndatetime(y,m,d+1,16)', end: 'dyndatetime(y,m,d+1,18)', title: 'Demo', resource: 5 },
+        { start: 'dyndatetime(y,m,d+2,15)', end: 'dyndatetime(y,m,d+2,17)', title: 'Retro', resource: 5 },
+        { start: 'dyndatetime(y,m,d+3,12)', end: 'dyndatetime(y,m,d+3,14)', title: 'Review', resource: 5 },
+        { start: 'dyndatetime(y,m,d-3,14)', end: 'dyndatetime(y,m,d-3,16)', title: 'Retro', resource: 6 },
+        { start: 'dyndatetime(y,m,d-2,13)', end: 'dyndatetime(y,m,d-2,15)', title: 'Update', resource: 6 },
+        { start: 'dyndatetime(y,m,d-1,9)', end: 'dyndatetime(y,m,d-1,11)', title: 'Meeting', resource: 6 },
+        { start: 'dyndatetime(y,m,d,8)', end: 'dyndatetime(y,m,d,10)', title: 'Stand-up', resource: 6 },
+        { start: 'dyndatetime(y,m,d+1,14)', end: 'dyndatetime(y,m,d+1,16)', title: 'Call', resource: 6 },
+        { start: 'dyndatetime(y,m,d+2,9)', end: 'dyndatetime(y,m,d+2,11)', title: 'Update', resource: 6 },
+        { start: 'dyndatetime(y,m,d+3,16)', end: 'dyndatetime(y,m,d+3,18)', title: 'Strategy', resource: 6 },
       ];
 
       var myEvents = [
@@ -107,15 +105,18 @@ export default {
       var myCalendar = $('#demo-control-the-column-width')
         .mobiscroll()
         .eventcalendar({
-          // context,
-          // drag,
-          cssClass: myCustomClass,
+          cssClass: 'mds-col-width-small',
           data: myResEvents,
           groupBy: 'date',
           resources: myResources,
           view: myView,
         })
         .mobiscroll('getInst');
+
+      $('input[name="mds-custom-width-options"]').on('change', function () {
+        myCalendar.setOptions({ cssClass: 'mds-col-width-' + $(this).val() });
+        myCalendar.navigate(new Date());
+      });
 
       $('input[name="mds-custom-width-resources-toggle"]').on('change', function () {
         myCalendar.setOptions({
@@ -124,51 +125,56 @@ export default {
         });
         myCalendar.navigate(new Date());
       });
-
-      $('input[name="mds-custom-width-options"]').on('change', function () {
-        myCalendar.setOptions({ cssClass: $(this).val() });
-        myCalendar.navigate(new Date());
-      });
     });
   },
   // eslint-disable-next-line es5/no-template-literals
   markup: `
-<div mbsc-segmented-group>
-  <label>
-    <input type="radio" mbsc-segmented name="mds-custom-width-options" value="my-col-width-small" checked>
-    50px
-  </label>
-  <label>
-    <input type="radio" mbsc-segmented name="mds-custom-width-options" value="my-col-width-medium">
-    100px
-  </label>
-  <label>
-  <input type="radio" mbsc-segmented name="mds-custom-width-options" value="my-col-width-large">
-    150px
-    </label>
+<div mbsc-page class="mds-full-height">
+  <div class="mbsc-flex-col mds-full-height">
+    <div mbsc-segmented-group class="mbsc-flex-none">
+      <label>
+        <input type="radio" mbsc-segmented name="mds-custom-width-options" value="small" checked>
+        50px
+      </label>
+      <label>
+        <input type="radio" mbsc-segmented name="mds-custom-width-options" value="medium">
+        100px
+      </label>
+      <label>
+        <input type="radio" mbsc-segmented name="mds-custom-width-options" value="large">
+        150px
+      </label>
+    </div>
+    <div mbsc-segmented-group class="mbsc-flex-none">
+      <label>
+        <input type="radio" mbsc-segmented name="mds-custom-width-resources-toggle" value="on" checked>
+        With resources
+      </label>
+      <label>
+        <input type="radio" mbsc-segmented name="mds-custom-width-resources-toggle" value="off">
+        Without resources
+      </label>
+    </div>
+    <div id="demo-control-the-column-width"></div>
   </div>
-  <div mbsc-segmented-group id="toggle-resources">
-  <label>
-    <input type="radio" mbsc-segmented name="mds-custom-width-resources-toggle" value="on" checked>
-    With resources
-  </label>
-  <label>
-    <input type="radio" mbsc-segmented name="mds-custom-width-resources-toggle" value="off">
-    Without resources
-  </label>
 </div>
-<div id="demo-control-the-column-width"></div>
-      `,
+`,
   // eslint-disable-next-line es5/no-template-literals
   css: `
-.my-col-width-small .mbsc-schedule-col-width {
+.mds-full-height {
+  height: 100%;
+}
+
+.mds-col-width-small .mbsc-schedule-col-width {
   width: 50px;
 } 
-.my-col-width-medium .mbsc-schedule-col-width {
+
+.mds-col-width-medium .mbsc-schedule-col-width {
   width: 100px;
-} 
-.my-col-width-large .mbsc-schedule-col-width {
+}
+
+.mds-col-width-large .mbsc-schedule-col-width {
   width: 150px;
-} 
-      `,
+}
+`,
 };
