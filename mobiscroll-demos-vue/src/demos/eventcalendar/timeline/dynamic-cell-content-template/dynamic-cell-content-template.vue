311f2e72a5cc461129ca3e6e56f1f2cf56736445
Lehel <deaklehels@gmail.com>
2025-06-02 07:42:46 +0300

    demo files generated, demo routes added

---
+++ mobiscroll-demos-vue/src/demos/eventcalendar/timeline/dynamic-cell-content-template/dynamic-cell-content-template.vue
@@ -0,0 +1,49 @@
+
+<script setup>
+import { getJson, MbscEventcalendar, setOptions /* localeImport */ } from '@mobiscroll/vue'
+import { onMounted, ref } from 'vue'
+
+setOptions({
+  // locale,
+  // theme
+})
+
+const myEvents = ref([])
+
+const myResources = [
+]
+
+const myView = {
+  timeline: {
+    allDay: false,
+    type: 'week',
+    startDay: 1,
+    endDay: 5,
+    startTime: '09:00',
+    endTime: '18:00'
+  }
+}
+
+onMounted(() => {
+  getJson(
+    'https://trial.mobiscroll.com/timeline-events/',
+    (events) => {
+      myEvents.value = events
+    },
+    'jsonp'
+  )
+})
+</script>
+
+<template>
+  <!-- dragOptions -->
+  <MbscEventcalendar
+    :view="myView"
+    :data="myEvents"
+    :resources="myResources"
+  />
+</template>
+
+<style>
+
+</style>
