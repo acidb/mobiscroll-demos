e474830f38e8fc49afe5617f52f04ba7a84b6c58
Nagy Gabriella <gabi@theacidmedia.net>
2025-07-25 09:54:44 +0300

    demo test

--- mobiscroll-demos-jquery/src/demos/eventcalendar/timeline/display-resource-information-on-hover/display-resource-information-on-hover.js
+++ mobiscroll-demos-jquery/src/demos/eventcalendar/timeline/display-resource-information-on-hover/display-resource-information-on-hover.js
@@ -64,7 +64,7 @@ export default {
           clearTimeout(closeTimer);
         }
         closeTimer = setTimeout(function () {
-          tooltip.close();
+          // tooltip.close();
           closeTimer = null;
         }, 200);
       }
