a58a1f7453ce31af2db6e431a8b6974674a6ed8e
Nagy Gabriella <gabi@theacidmedia.net>
2025-09-10 12:39:09 +0300

    demo updates

--- mobiscroll-demos-angular/src/demos/eventcalendar/scheduler/hide-non-working-resources/hide-non-working-resources.ts
+++ mobiscroll-demos-angular/src/demos/eventcalendar/scheduler/hide-non-working-resources/hide-non-working-resources.ts
@@ -1,6 +1,5 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-import { MbscEventcalendarOptions, setOptions /* localeImport */ } from '@mobiscroll/angular';
+import { Component } from '@angular/core';
+import { MbscCalendarEvent, MbscEventcalendarView, MbscResource, setOptions /* localeImport */ } from '@mobiscroll/angular';
 
 setOptions({
   // locale,
@@ -8,33 +7,23 @@ setOptions({
 });
 
 @Component({
-  selector: 'app-scheduler-show-hide-hours-days',
-  templateUrl: './show-hide-hours-days.html',
+  selector: 'app-scheduler-hide-non-working-resources',
+  templateUrl: './hide-non-working-resources.html',
   standalone: false,
 })
-export class AppComponent implements OnInit {
-  constructor(private http: HttpClient) {}
-
-  events: any;
-
-  eventSettings: MbscEventcalendarOptions = {
-    // drag,
-    view: {
-      schedule: {
-        type: 'week',
-        startDay: 1,
-        endDay: 5,
-        startTime: '09:00',
-        endTime: '18:00',
-        timeCellStep: 30,
-        timeLabelStep: 30,
-      },
-    },
-  };
-
-  ngOnInit() {
-    this.http.jsonp('https://trial.mobiscroll.com//workday-events/?vers=5', 'callback').subscribe((resp: any) => {
-      this.events = resp;
-    });
+export class AppComponent {
+  myView: MbscEventcalendarView = {
+    schedule: {
+      type: 'week',
+      allDay: false,
+      startDay: 1,
+      endDay: 5,
+      startTime: '08:00',
+      endTime: '20:00',
+      hideInvalidColumns: true,
+    }
   }
+
+  myEvents: MbscCalendarEvent[] = []
+  myResources: MbscResource[] = []
 }
