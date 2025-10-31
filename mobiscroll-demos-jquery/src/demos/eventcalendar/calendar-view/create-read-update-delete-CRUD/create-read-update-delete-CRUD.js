7214671a84c79869190a963466df368f01f14bcc
Nagy Gabriella <gabi@theacidmedia.net>
2025-10-31 09:35:28 +0200

    code format fix for jq and js

--- mobiscroll-demos-jquery/src/demos/eventcalendar/calendar-view/create-read-update-delete-CRUD/create-read-update-delete-CRUD.js
+++ mobiscroll-demos-jquery/src/demos/eventcalendar/calendar-view/create-read-update-delete-CRUD/create-read-update-delete-CRUD.js
@@ -378,157 +378,159 @@ export default {
 <div id="demo-crud-event-calendar"></div>
 
 <div style="display: none">
-    <div id="crud-add-edit-popup">
-        <div class="mbsc-form-group">
-            <label>
-            Title
-            <input mbsc-input id="crud-popup-event-title">
-        </label>
-            <label>
-            Description
-            <textarea mbsc-textarea id="crud-popup-event-desc"></textarea>
-        </label>
-        </div>
-        <div class="mbsc-form-group">
-            <label>
-            All-day
-            <input mbsc-switch id="crud-popup-event-all-day" type="checkbox" />
-        </label>
-            <label>
-            Starts
-            <input mbsc-input id="crud-popup-event-start" />
-        </label>
-            <label>
-            Ends
-            <input mbsc-input id="crud-popup-event-end" />
-        </label>
-        <label id="travel-time-group">
-            <select data-label="Travel time" mbsc-dropdown id="crud-popup-event-buffer">
-                <option value="0">None</option>
-                <option value="5">5 minutes</option>
-                <option value="15">15 minutes</option>
-                <option value="30">30 minutes</option>
-                <option value="60">1 hour</option>
-                <option value="90">1.5 hours</option>
-                <option value="120">2 hours</option>
-            </select>
-        </label>
-            <div id="crud-popup-event-dates"></div>
-            <div id="crud-event-color-picker" class="mbsc-flex mds-crud-event-color-cont">
-                <div class="mbsc-flex-1-0">Color</div>
-                <div id="crud-popup-event-color">
-                    <div id="crud-popup-event-color-preview" class="mds-crud-selected-event-color"></div>
-                </div>
-            </div>
-            <label>
-            Show as busy
-            <input id="crud-popup-event-status-busy" mbsc-segmented type="radio" name="event-status" value="busy">
-        </label>
-            <label>
-            Show as free
-            <input id="crud-popup-event-status-free" mbsc-segmented type="radio" name="event-status" value="free">
-        </label>
-            <div class="mbsc-button-group">
-                <button class="mbsc-button-block" id="crud-popup-event-delete" mbsc-button data-color="danger" data-variant="outline">Delete event</button>
-            </div>
-        </div>
+  <div id="crud-add-edit-popup">
+    <div class="mbsc-form-group">
+      <label>
+        Title
+        <input mbsc-input id="crud-popup-event-title" />
+      </label>
+      <label>
+        Description
+        <textarea mbsc-textarea id="crud-popup-event-desc"></textarea>
+      </label>
     </div>
+    <div class="mbsc-form-group">
+      <label>
+        All-day
+        <input mbsc-switch id="crud-popup-event-all-day" type="checkbox" />
+      </label>
+      <label>
+        Starts
+        <input mbsc-input id="crud-popup-event-start" />
+      </label>
+      <label>
+        Ends
+        <input mbsc-input id="crud-popup-event-end" />
+      </label>
+      <label id="travel-time-group">
+        <select data-label="Travel time" mbsc-dropdown id="crud-popup-event-buffer">
+          <option value="0">None</option>
+          <option value="5">5 minutes</option>
+          <option value="15">15 minutes</option>
+          <option value="30">30 minutes</option>
+          <option value="60">1 hour</option>
+          <option value="90">1.5 hours</option>
+          <option value="120">2 hours</option>
+        </select>
+      </label>
+      <div id="crud-popup-event-dates"></div>
+      <div id="crud-event-color-picker" class="mbsc-flex mds-crud-event-color-cont">
+        <div class="mbsc-flex-1-0">Color</div>
+        <div id="crud-popup-event-color">
+          <div id="crud-popup-event-color-preview" class="mds-crud-selected-event-color"></div>
+        </div>
+      </div>
+      <label>
+        Show as busy
+        <input id="crud-popup-event-status-busy" mbsc-segmented type="radio" name="event-status" value="busy" />
+      </label>
+      <label>
+        Show as free
+        <input id="crud-popup-event-status-free" mbsc-segmented type="radio" name="event-status" value="free" />
+      </label>
+      <div class="mbsc-button-group">
+        <button class="mbsc-button-block" id="crud-popup-event-delete" mbsc-button data-color="danger" data-variant="outline">
+          Delete event
+        </button>
+      </div>
+    </div>
+  </div>
 
-    <div id="crud-color-picker-popup">
-        <div class="mbsc-flex mds-crud-color-row">
-            <div class="mds-crud-color-value" data-value="#ffeb3c">
-                <div class="mds-crud-color mbsc-icon mbsc-font-icon mbsc-icon-material-check" style="background:#ffeb3c"></div>
-            </div>
-            <div class="mds-crud-color-value" data-value="#ff9900">
-                <div class="mds-crud-color mbsc-icon mbsc-font-icon mbsc-icon-material-check" style="background:#ff9900"></div>
-            </div>
-            <div class="mds-crud-color-value" data-value="#f44437">
-                <div class="mds-crud-color mbsc-icon mbsc-font-icon mbsc-icon-material-check" style="background:#f44437"></div>
-            </div>
-            <div class="mds-crud-color-value" data-value="#ea1e63">
-                <div class="mds-crud-color mbsc-icon mbsc-font-icon mbsc-icon-material-check" style="background:#ea1e63"></div>
-            </div>
-            <div class="mds-crud-color-value" data-value="#9c26b0">
-                <div class="mds-crud-color mbsc-icon mbsc-font-icon mbsc-icon-material-check" style="background:#9c26b0"></div>
-            </div>
-        </div>
-        <div class="mbsc-flex mds-crud-color-row">
-            <div class="mds-crud-color-value" data-value="#3f51b5">
-                <div class="mds-crud-color mbsc-icon mbsc-font-icon mbsc-icon-material-check" style="background:#3f51b5"></div>
-            </div>
-            <div class="mds-crud-color-value" data-value="">
-                <div class="mds-crud-color mbsc-icon mbsc-font-icon mbsc-icon-material-check"></div>
-            </div>
-            <div class="mds-crud-color-value" data-value="#009788">
-                <div class="mds-crud-color mbsc-icon mbsc-font-icon mbsc-icon-material-check" style="background:#009788"></div>
-            </div>
-            <div class="mds-crud-color-value" data-value="#4baf4f">
-                <div class="mds-crud-color mbsc-icon mbsc-font-icon mbsc-icon-material-check" style="background:#4baf4f"></div>
-            </div>
-            <div class="mds-crud-color-value" data-value="#7e5d4e">
-                <div class="mds-crud-color mbsc-icon mbsc-font-icon mbsc-icon-material-check" style="background:#7e5d4e"></div>
-            </div>
-        </div>
+  <div id="crud-color-picker-popup">
+    <div class="mbsc-flex mds-crud-color-row">
+      <div class="mds-crud-color-value" data-value="#ffeb3c">
+        <div class="mds-crud-color mbsc-icon mbsc-font-icon mbsc-icon-material-check" style="background: #ffeb3c"></div>
+      </div>
+      <div class="mds-crud-color-value" data-value="#ff9900">
+        <div class="mds-crud-color mbsc-icon mbsc-font-icon mbsc-icon-material-check" style="background: #ff9900"></div>
+      </div>
+      <div class="mds-crud-color-value" data-value="#f44437">
+        <div class="mds-crud-color mbsc-icon mbsc-font-icon mbsc-icon-material-check" style="background: #f44437"></div>
+      </div>
+      <div class="mds-crud-color-value" data-value="#ea1e63">
+        <div class="mds-crud-color mbsc-icon mbsc-font-icon mbsc-icon-material-check" style="background: #ea1e63"></div>
+      </div>
+      <div class="mds-crud-color-value" data-value="#9c26b0">
+        <div class="mds-crud-color mbsc-icon mbsc-font-icon mbsc-icon-material-check" style="background: #9c26b0"></div>
+      </div>
     </div>
+    <div class="mbsc-flex mds-crud-color-row">
+      <div class="mds-crud-color-value" data-value="#3f51b5">
+        <div class="mds-crud-color mbsc-icon mbsc-font-icon mbsc-icon-material-check" style="background: #3f51b5"></div>
+      </div>
+      <div class="mds-crud-color-value" data-value="">
+        <div class="mds-crud-color mbsc-icon mbsc-font-icon mbsc-icon-material-check"></div>
+      </div>
+      <div class="mds-crud-color-value" data-value="#009788">
+        <div class="mds-crud-color mbsc-icon mbsc-font-icon mbsc-icon-material-check" style="background: #009788"></div>
+      </div>
+      <div class="mds-crud-color-value" data-value="#4baf4f">
+        <div class="mds-crud-color mbsc-icon mbsc-font-icon mbsc-icon-material-check" style="background: #4baf4f"></div>
+      </div>
+      <div class="mds-crud-color-value" data-value="#7e5d4e">
+        <div class="mds-crud-color mbsc-icon mbsc-font-icon mbsc-icon-material-check" style="background: #7e5d4e"></div>
+      </div>
+    </div>
+  </div>
 </div>
   `,
   // eslint-disable-next-line es5/no-template-literals
   css: `
 .mds-crud-event-color-cont {
-    margin: 16px;
-    align-items: center;
-    cursor: pointer;
+  margin: 16px;
+  align-items: center;
+  cursor: pointer;
 }
 
 .mds-crud-selected-event-color {
-    width: 30px;
-    height: 30px;
-    border-radius: 15px;
-    margin-right: 10px;
-    margin-left: 240px;
-    background: #5ac8fa;
+  width: 30px;
+  height: 30px;
+  border-radius: 15px;
+  margin-right: 10px;
+  margin-left: 240px;
+  background: #5ac8fa;
 }
 
 .mds-crud-color-row {
-    justify-content: center;
-    margin: 5px;
+  justify-content: center;
+  margin: 5px;
 }
 
 .mds-crud-color {
-    position: relative;
-    min-width: 46px;
-    min-height: 46px;
-    margin: 2px;
-    cursor: pointer;
-    border-radius: 23px;
-    background: #5ac8fa;
+  position: relative;
+  min-width: 46px;
+  min-height: 46px;
+  margin: 2px;
+  cursor: pointer;
+  border-radius: 23px;
+  background: #5ac8fa;
 }
 
 .mds-crud-color:before {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    margin-top: -10px;
-    margin-left: -10px;
-    color: #f7f7f7;
-    font-size: 20px;
-    text-shadow: 0 0 3px #000;
-    display: none;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  margin-top: -10px;
+  margin-left: -10px;
+  color: #f7f7f7;
+  font-size: 20px;
+  text-shadow: 0 0 3px #000;
+  display: none;
 }
 
 .mds-crud-color-value {
-    padding: 3px;
-    margin: 2px;
+  padding: 3px;
+  margin: 2px;
 }
 
 .mds-crud-color-value.selected,
 .mds-crud-color-value:hover {
-    box-shadow: inset 0 0 0 3px #007bff;
-    border-radius: 48px;
+  box-shadow: inset 0 0 0 3px #007bff;
+  border-radius: 48px;
 }
 
 .mds-crud-color-value.selected .mds-crud-color:before {
-    display: block;
+  display: block;
 }
   `,
 };
